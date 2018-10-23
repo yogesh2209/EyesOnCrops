cecf868569ebadee907204bab11e8e29e5da63ad
yogesh2209 <yogesh22kohli@gmail.com>
2018-10-22 23:13:48 -0700

    Webview work going

--- EyesOnCrops/Controller/MiscController/Terms&PrivacyController/ETermsPrivacyConmonViewController.swift
+++ EyesOnCrops/Controller/MiscController/Terms&PrivacyController/ETermsPrivacyConmonViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import WebKit
 
-class ETermsPrivacyConmonViewController: EBaseViewController {
+class ETermsPrivacyConmonViewController: EBaseViewController, WKNavigationDelegate {
     @IBOutlet weak var webView: WKWebView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var buttonBack: UIButton!
     var indicatorVariable : String!
 
@@ -32,17 +33,41 @@ class ETermsPrivacyConmonViewController: EBaseViewController {
     
     //MARK: Private Methods
     func loadWebView() {
+        
+        var urlString = ""
+        
         if indicatorVariable == "TERMS" {
-            
+            urlString = MAIN_URL + TERMSOFUSE_URL
         }
         else if indicatorVariable == "PRIVACY" {
-            
+            urlString = MAIN_URL + PRIVACY_POLICY_URL
         }
         //setup webview here
         
-        
-        
+        guard let url = URL(string: urlString) else { return }
+        webView.navigationDelegate = self
+        webView.load(URLRequest(url: url))
     }
+    
+    //MARK: WKWebView Delegates
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+        self.alertMessage(title: ALERT_TITLE, message: SOMETHING_WENT_WRONG_ERROR)
+    }
+
+    
     //MARK: UIButton Actions
     @IBAction func buttonBackPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
